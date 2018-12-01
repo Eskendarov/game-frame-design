bd0752653b220cb785df4fb6ffa60dd931f56713
Eskendarov <envereskendarov@gmail.com>
2018-12-01 06:56:35 +0300

    Урок 3. Разработка «каркаса» игры

--- desktop/src/ru/eskendarov/desktop/DesktopLauncher.java
+++ desktop/src/ru/eskendarov/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package ru.eskendarov.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import ru.eskendarov.Star2DGame;
 import ru.eskendarov.StarGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new StarGame(), config);
+		config.width = 600;
+		config.height = 600;
+		new LwjglApplication(new Star2DGame(), config);
 	}
 }
